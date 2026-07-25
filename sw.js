6c15f1b8b0699338572337764915cf51d4389c87
syunnjack <syunnjack@gmail.com>
2026-07-25 21:09:00 +0900

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "shinku-trainer-v1";
+const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
+
