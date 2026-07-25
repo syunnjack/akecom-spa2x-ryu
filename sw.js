bf4e43a893fc6c5c46e3dca503851abe7b2192b2
syunnjack <syunnjack@gmail.com>
2026-07-26 04:02:24 +0900

    Update sw.js for buffered input

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "shinku-trainer-v1";
+const CACHE = "shinku-trainer-v2";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
@@ -14,4 +14,3 @@ self.addEventListener("activate", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
 });
-
