064cce31980ae233b86462ea215af6351bfbc1fc
syunnjack <syunnjack@gmail.com>
2026-07-27 01:31:21 +0900

    入力フレームタイムラインを追加 (#5)
    * Update README.md for input timeline
    
    * Update index.html for input timeline
    
    * Update style.css for input timeline
    
    * Add frame-by-frame input timeline
    
    * Update sw.js for input timeline

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "shinku-trainer-v3";
+const CACHE = "shinku-trainer-v4";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
