d265a720bd1f8154a0a6fc3fe9f4cf3ba3fce869
syunnjack <syunnjack@gmail.com>
2026-07-28 02:46:27 +0900

    参考動画に合わせてキャンセル入力判定を改善 (#6)
    * 参考動画に合わせて入力完成判定を改善
    
    * 参考動画に合わせて入力完成判定を改善
    
    * 参考動画に合わせて入力完成判定を改善
    
    * 参考動画に合わせて入力完成判定を改善
    
    * 参考動画に合わせて入力完成判定を改善

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "shinku-trainer-v4";
+const CACHE = "shinku-trainer-v5";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
